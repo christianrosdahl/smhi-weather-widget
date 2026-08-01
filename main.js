cf803e1af0d05a8ebb28f2b7609352ba00a84743
Christian Rosdahl <christian.rosdahl@gmail.com>
2026-08-01 10:39:04 +0200

    Add comments to emoji mapping

--- main.js
+++ main.js
@@ -576,33 +576,33 @@ function getWeatherEmoji(code, night) {
   }
 
   const map = {
-    1: "☀️",
-    2: "🌤️",
-    3: "⛅",
-    4: "⛅",
-    5: "🌥️",
-    6: "☁️",
-    7: "🌫️",
-    8: "🌦️",
-    9: "🌦️",
-    10: "🌦️",
-    11: "⛈️",
-    12: "🌨️",
-    13: "🌨️",
-    14: "🌨️",
-    15: "🌨️",
-    16: "🌨️",
-    17: "🌨️",
-    18: "🌧️",
-    19: "🌧️",
-    20: "🌧️",
-    21: "🌩️",
-    22: "🌨️",
-    23: "🌨️",
-    24: "🌨️",
-    25: "🌨️",
-    26: "🌨️",
-    27: "🌨️",
+    1: "☀️", // Clear sky
+    2: "🌤️", // Nearly clear sky
+    3: "⛅", // Variable cloudiness
+    4: "⛅", // Halfclear sky
+    5: "🌥️", // Cloudy sky
+    6: "☁️", // Overcast
+    7: "🌫️", // Fog
+    8: "🌦️", // Light rain showers
+    9: "🌦️", // Moderate rain showers
+    10: "🌦️", // Heavy rain showers
+    11: "⛈️", // Thunderstorm
+    12: "🌨️", // Light sleet showers
+    13: "🌨️", // Moderate sleet showers
+    14: "🌨️", // Heavy sleet showers
+    15: "🌨️", // Light snow showers
+    16: "🌨️", // Moderate snow showers
+    17: "🌨️", // Heavy snow showers
+    18: "🌧️", // Light rain
+    19: "🌧️", // Moderate rain
+    20: "🌧️", // Heavy rain
+    21: "🌩️", // Thunder
+    22: "🌨️", // Light sleet
+    23: "🌨️", // Moderate sleet
+    24: "🌨️", // Heavy sleet
+    25: "🌨️", // Light snowfall
+    26: "🌨️", // Moderate snowfall
+    27: "🌨️", // Heavy snowfall
   };
   return map[code] || "🌤️";
 }
